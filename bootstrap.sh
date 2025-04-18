eb654254933c84073b6828a22bff9f623d667956
Jayadeep KM <kmjayadeep@gmail.com>
2025-04-18 20:47:30 +0200

    dotfiles for taskwarrior
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,9 @@ sudo apt-get install -y neovim zsh
 # OhMyZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sudo chsh -s /bin/zsh $USER
+cp dotfiles/zshrc ~/.zshrc
 
 # Taskwarrior
+nix-env -f "<nixpkgs>" -iA taskwarrior3
 mkdir -p ~/.config/task
 cp dotfiles/taskrc ~/.config/task/taskrc
