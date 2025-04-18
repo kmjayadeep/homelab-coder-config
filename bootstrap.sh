c007e06c807687ea2ba454abdf3bee23d34c1e4f
Jayadeep KM <kmjayadeep@gmail.com>
2025-04-18 20:33:17 +0200

    add taskwarrior to dotfiles
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,7 @@ sudo apt-get install -y neovim zsh
 # OhMyZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sudo chsh -s /bin/zsh $USER
+
+# Taskwarrior
+mkdir -p ~/.config/task
+cp dotfiles/taskrc ~/.config/task/taskrc
